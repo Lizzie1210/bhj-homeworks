2d7edadbf87e122005e536f68b7d29ab1a9f1174
Elizaveta M <elizaveta.deb1210@yandex.ru>
2025-10-16 13:27:20 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -16,16 +16,23 @@ class Game {
     this.lossElement.textContent = 0;
   }
 
-  registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
+registerEvents() {
+  document.addEventListener('keydown', (event) => {
+    const currentChar = this.currentSymbol.textContent.toLowerCase();
+    const pressedKey = event.key.toLowerCase();
+
+    if (pressedKey.length !== 1) {
+      return;
+    }
+
+    if (pressedKey === currentChar) {
+      this.success();
+    } else {
+      this.fail();
+    }
+  });
+}
+
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
