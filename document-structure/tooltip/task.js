44f62e59c3e9f96f3e14178376205299142f3e35
Elizaveta M <elizaveta.deb1210@yandex.ru>
2025-10-29 18:50:37 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,25 +4,29 @@ const tooltipContainer = document.createElement('div');
 tooltipContainer.classList.add('tooltip');
 document.body.appendChild(tooltipContainer);
 
-function showTooltip(e) {
-    const tooltipText = e.target.getAttribute('title');
-    
+function showTooltip(event) {
+    event.preventDefault(); 
+
+    const target = event.target;
+    const tooltipText = target.getAttribute('title');
+
+    if (tooltipContainer.textContent === tooltipText && tooltipContainer.classList.contains('tooltip_active')) {
+        tooltipContainer.classList.remove('tooltip_active');
+        return;
+    }
+
     tooltipContainer.textContent = tooltipText;
     tooltipContainer.classList.add('tooltip_active');
-    
-    const rect = e.target.getBoundingClientRect();
-    const left = rect.left + rect.width + 10;
-    const top = rect.top - 10;
-    
+
+    const rect = target.getBoundingClientRect();
+
+    const left = rect.left;
+    const top = rect.bottom + 5; 
+
     tooltipContainer.style.left = `${left}px`;
     tooltipContainer.style.top = `${top}px`;
 }
 
-function hideTooltip() {
-    tooltipContainer.classList.remove('tooltip_active');
-}
-
 tooltips.forEach(tooltip => {
     tooltip.addEventListener('click', showTooltip);
-    tooltip.addEventListener('mouseleave', hideTooltip);
 });
